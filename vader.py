e6225698bd8a9b282dcd369cd7b78b91003ee8fa
rodsmann <122959455+rodsmann@users.noreply.github.com>
2023-02-01 20:17:45 +0800

    added try catch

--- vader.py
+++ vader.py
@@ -13,30 +13,31 @@ async def index():
 
 @vader_api.post("/analyze")
 async def analyze(payload: dict = Body(...)):
-    # data = pd.read_csv("senti.csv")
-    sid_obj = SentimentIntensityAnalyzer() 
+    try:
+        # data = pd.read_csv("senti.csv")
+        sid_obj = SentimentIntensityAnalyzer() 
 
-    data = payload["scraped"]
+        data = payload["scraped"]
 
-    sentiment_dict = sid_obj.polarity_scores(data) 
-    tokenized_sentence = nltk.word_tokenize(data)
-    pos_word_list=[]
-    neu_word_list=[]
-    neg_word_list=[]
+        sentiment_dict = sid_obj.polarity_scores(data) 
+        tokenized_sentence = nltk.word_tokenize(data)
+        pos_word_list=[]
+        neu_word_list=[]
+        neg_word_list=[]
 
-    for word in tokenized_sentence:
-        if (sid_obj.polarity_scores(word)['compound']) >= 0.1:
-            pos_word_list.append(word)
-        elif (sid_obj.polarity_scores(word)['compound']) <= -0.1:
-            neg_word_list.append(word)
-        else:
-            neu_word_list.append(word)
+        for word in tokenized_sentence:
+            if (sid_obj.polarity_scores(word)['compound']) >= 0.1:
+                pos_word_list.append(word)
+            elif (sid_obj.polarity_scores(word)['compound']) <= -0.1:
+                neg_word_list.append(word)
+            else:
+                neu_word_list.append(word)
 
-    return {
-            "sentiment": sentiment_dict,
-            "positive-words":pos_word_list,
-            "neutral-words":neu_word_list,
-            "negative-words":neg_word_list      
-            }
-              
-
+        return {
+                "sentiment": sentiment_dict,
+                "positive-words":pos_word_list,
+                "neutral-words":neu_word_list,
+                "negative-words":neg_word_list      
+                }
+    except Exception as e:
+        return { "message": str(e) }
