22f01ab5d42f56bd8836a1561292781f0a3f3e92
rodsmann <122959455+rodsmann@users.noreply.github.com>
2023-02-04 00:55:52 +0800

    add wordcloud

--- vader.py
+++ vader.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import io
+import numpy as np
+import urllib
 import nltk
 nltk.download('punkt')
 # import pandas as pd
@@ -34,11 +39,36 @@ async def analyze(payload: dict = Body(...)):
             else:
                 neu_word_list.append(word)
 
+                
+        wordcloud = WordCloud().generate(" ".join(pos_word_list))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        
+        # Save the image to a buffer
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+
+        pos_image = urllib.parse.quote(buffer.getvalue().hex())
+        
+        wordcloud = WordCloud().generate(" ".join(neg_word_list))
+        plt.imshow(wordcloud, interpolation='bilinear')
+        plt.axis("off")
+        
+        # Save the image to a buffer
+        buffer = io.BytesIO()
+        plt.savefig(buffer, format='png')
+        buffer.seek(0)
+
+        neg_image = urllib.parse.quote(buffer.getvalue().hex())
+
         return {
                 "sentiment": sentiment_dict,
                 "positive-words":pos_word_list,
                 "negative-words":neg_word_list,
-                "neutral-words":neu_word_list    
+                "neutral-words":neu_word_list,
+                "pos-image" : pos_image,
+                "neg-image" : neg_image
                 }
     except Exception as e:
         return { "message": str(e) }
